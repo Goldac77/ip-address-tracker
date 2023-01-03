6b8f05549577ec2f267b2f418911b96a4382f20f
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-01-03 20:26:59 +0000

    added a component for displaying api data

---
+++ src/Data.js
@@ -0,0 +1,24 @@
+function Data(props) {
+    return (
+        <div className="main-div">
+            <div className="ip--address">
+                <h3>IP ADDRESS</h3>
+                {props.ip}
+            </div>
+            <div className="location">
+                <h3>LOCATION</h3>
+                {props.location}
+            </div>
+            <div className="timezone">
+                <h3>TIMEZONE</h3>
+                UTC{props.timezone}
+            </div>
+            <div className="isp">
+                <h3>ISP</h3>
+                {props.isp}
+            </div>
+        </div>
+    )
+}
+
+export default Data
