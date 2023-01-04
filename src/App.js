9d25f641c9d4fed4978531d37963205c596e3bd5
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-01-04 23:21:55 +0000

    changed fetch end point

--- src/App.js
+++ src/App.js
@@ -9,19 +9,13 @@ function App() {
     ip_address: ""
   })
   const [apiData, setApiData] = useState(null)
-  let ipAddress = useRef("")
-  
 
-  const requestOne = axios.get('https://api.ipify.org?format=json')
-  const requestTwo = axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_8J8AJvFhxoGg3zlFER17GFYquOMiu&ipAddress=${ipAddress.current}`)
-
-  //Receive the client's IP Address from ipify api AND Receive Geolocation data from api
+  //Receive the client's IP Address info from ipwhois
   useEffect(() => {
-    axios.all([requestOne, requestTwo])
-    .then(axios.spread((...responses) => {
-      ipAddress.current = responses[0].data.ip
-      setApiData(responses[1].data)
-    }))
+    axios.get("http://ipwho.is/")
+    .then((response) => {
+      setApiData(response.data)
+    })
   }, [])
 
   function handleChange(event) {
@@ -39,14 +33,13 @@ function App() {
   }
 
   console.log(formData)
-  console.log(ipAddress.current)
   console.log(apiData)
 
   if(apiData == null){
     return <p>Loading...</p>
   } else {
 
-    const position = [apiData.location.lat, apiData.location.lng]
+    const position = [apiData.latitude, apiData.longitude]
 
     return (
       <div className="App">
@@ -58,14 +51,13 @@ function App() {
   
         <Data
           ip={apiData.ip}
-          region={apiData.location.region}
-          country={apiData.location.country}
-          timezone={apiData.location.timezone}
-          isp={apiData.isp}
+          region={apiData.region}
+          country={apiData.country}
+          timezone={apiData.timezone.utc}
+          isp={apiData.connection.isp}
         />
   
         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-          
           <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
